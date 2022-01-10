130a9da56039696b5945f6a45e08bed4c69864fa
jwywrocki <jaqb1991@gmail.com>
2022-01-10 19:46:07 +0100

    js cleaning

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// Scroll top
-document.querySelector(".scrollTop").addEventListener('click', () => {
-    document.querySelector("header").scrollIntoView({ behavior: 'smooth' });
-})
-
-// Scroll to next anchor on mouse wheel action
-
