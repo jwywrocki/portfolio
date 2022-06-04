60f90a0142d304ac3b05286bb3656aa0aa673ee6
jwywrocki <jaqb1991@gmail.com>
2022-06-04 23:10:33 +0200

    changed to mobile first with some adjustments

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var scrollPosition = window.pageYOffset;
 var root = getComputedStyle(document.querySelector(":root"));
 window.onscroll = function () {
     var currentScrollPosition = window.pageYOffset;
-    if (screen.width > 1000) {
+    if (window.screen.width >= 768) {
         scrollPosition > currentScrollPosition
             ? (document.querySelector(".navigation").style.top = "0")
             : (document.querySelector(".navigation").style.top =
