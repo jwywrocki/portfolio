b2b5f204b6a7d4116796ee235c751e0a2fe5a2b2
jwywrocki <jaqb1991@gmail.com>
2022-07-17 17:06:24 +0200

    dark light mode, and layout adjustments

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
-var scrollPosition = window.pageYOffset;
-var root = getComputedStyle(document.querySelector(":root"));
+let toggle = document.querySelector('#toggle');
+let scrollPosition = window.pageYOffset;
+
+toggle.addEventListener('click', themeSwitch);
+let root = getComputedStyle(document.querySelector(':root'));
+
 window.onscroll = function () {
-    var currentScrollPosition = window.pageYOffset;
+    let currentScrollPosition = window.pageYOffset;
     if (window.screen.width >= 768) {
         scrollPosition > currentScrollPosition
-            ? (document.querySelector(".navigation").style.top = "0")
-            : (document.querySelector(".navigation").style.top =
-                  root.getPropertyValue("--navHeightNegative"));
+            ? (document.querySelector('.navigation').style.top = '0')
+            : (document.querySelector('.navigation').style.top =
+                  root.getPropertyValue('--navHeightNegative'));
         scrollPosition = currentScrollPosition;
     } else {
         scrollPosition > currentScrollPosition
-            ? (document.querySelector(".navigation").style.bottom = "0")
-            : (document.querySelector(".navigation").style.bottom =
-                  root.getPropertyValue("--navHeightNegative"));
+            ? (document.querySelector('.navigation').style.bottom = '0')
+            : (document.querySelector('.navigation').style.bottom =
+                  root.getPropertyValue('--navHeightNegative'));
         scrollPosition = currentScrollPosition;
         // window.innerHeight + currentScrollPosition >= document.body.offsetHeight
         //     ? (document.querySelector(".navigation").style.bottom = "0")
         //     : null;
     }
 };
+
+function themeSwitch() {
+    toggle.classList.contains('fa-moon')
+        ? toggle.classList.replace('fa-moon', 'fa-sun')
+        : toggle.classList.replace('fa-sun', 'fa-moon');
+    document.body.classList.toggle('dark__theme');
+}
